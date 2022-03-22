8e7c575c1f1f1c9cc6c2622f91ffc6c35c25b712
sheehui <tansheehui@gmail.com>
2022-03-22 11:09:15 +0800

    Create base map

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
+import React, { useState } from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [currCoords, setCurrCoords] = useState([1.363649, 103.806181]); 
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+  const mapStyle = {
+    height: `${height}px`, 
+    width: `${width}`,
+    display: `flex`,
+    justifyContent: `center`,
+    margin: `auto`,
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <MapContainer center={currCoords} zoom={13} scrollWheelZoom={false} style={mapStyle}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
   );
 }
 
