9db910e8e4b46990ae5892dfe66e0f67458fdc2d
Royce Ho <royce963@gmail.com>
2022-04-23 21:01:37 +0800

    create athena queries file

---
+++ athena/queries.sql
@@ -0,0 +1,32 @@
+-- rainfall table
+CREATE EXTERNAL TABLE IF NOT EXISTS rainfall (
+  station_id STRING,
+  value FLOAT,
+  -- unable to parse ISO datetime, hence read as string into the table then use Presto to cast to timestamp 
+  -- https://aws.amazon.com/premiumsupport/knowledge-center/query-table-athena-timestamp-empty/
+  datetime STRING 
+  ) 
+  ROW FORMAT DELIMITED -- means that Athena will use a default library called LazySimpleSerDe to do the actual work of parsing the data
+  FIELDS TERMINATED BY ',' -- fields are comma separated 
+  LINES TERMINATED BY '\n' 
+  LOCATION 's3://cs4225-rainfall/outputs/'
+  TBLPROPERTIES ("skip.header.line.count"="1");
+
+-- rainfall query
+SELECT station_id, value, from_iso8601_timestamp(datetime) AS datetime FROM rainfall
+
+-- estimatedTravelTime table
+CREATE EXTERNAL TABLE IF NOT EXISTS estimatedTravelTime (
+  StartPoint STRING,
+  EndPoint FLOAT,
+  EstTime INTEGER,
+  datetime STRING
+  ) 
+  ROW FORMAT DELIMITED
+  FIELDS TERMINATED BY ','
+  LINES TERMINATED BY '\n'
+  LOCATION 's3://cs4225-estimated-travel-times/outputs/'
+  TBLPROPERTIES ("skip.header.line.count"="1");
+
+-- estimatedTravelTime query
+SELECT EstTime, from_iso8601_timestamp(datetime) AS datetime FROM estimatedTravelTime
